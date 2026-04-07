00509a90e23786758355f193b811269947e4029c
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-07 22:39:01 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/dockerspring/Dockerspring5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dockerspring5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
