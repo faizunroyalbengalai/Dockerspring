2dd554502a080896baf48235f7d4b90d08201a00
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-07 22:39:06 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/dockerspring/Dockerspring5Application.java
@@ -0,0 +1,13 @@
+package com.example.dockerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockerspring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dockerspring5Application.class, args);
+	}
+
+}
